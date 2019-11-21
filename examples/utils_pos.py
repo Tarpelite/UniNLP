2ece041aa5f68017bb21c7e22c74ad6343645f91
Tarpelite <tarpelite_chan@foxmail.com>
2019-11-21 17:00:20 +0800

    fix bugs

--- examples/utils_pos.py
+++ examples/utils_pos.py
@@ -36,11 +36,10 @@ class UDExample(object):
 
 class InputFeatures(object):
 
-    def __init__(self, unique_id, 
+    def __init__(self, 
                 input_ids, 
                 input_mask, segment_ids, label_ids):
         
-        self.unique_id = unique_id
         self.input_ids = input_ids
         self.input_mask = input_mask
         self.segment_ids = segment_ids
