e78f3db0eebd98930886b540e983250102a62700
Tarpelite <tarpelite_chan@foxmail.com>
2019-11-25 17:47:51 +0800

    fix bugs

--- examples/run_ner.py
+++ examples/run_ner.py
@@ -441,8 +441,7 @@ def main():
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
                                                 do_lower_case=args.do_lower_case,
                                                 cache_dir=args.cache_dir if args.cache_dir else None)
-    if args.config_name == "bert-large-cased":
-        model_class.bert.embeddings.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+    
     model = model_class.from_pretrained(args.model_name_or_path,
                                         from_tf=bool(".ckpt" in args.model_name_or_path),
                                         config=config,
