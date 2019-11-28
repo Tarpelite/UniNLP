63598145ab587ecbdd04b81f162e78c7279f5143
Tarpelite <tarpelite_chan@foxmail.com>
2019-11-28 13:49:31 +0800

    fix bugs

--- examples/run_mtdnn.py
+++ examples/run_mtdnn.py
@@ -596,7 +596,7 @@ def main():
     # Training
     if args.do_train:
         train_dataset = load_and_cache_train_examples(args, tokenizer, labels_pos, labels_ner, pad_token_label_id)
-        global_step, tr_loss = train(args, train_dataset, model, tokenizer, pad_token_label_id)
+        global_step, tr_loss = train(args, train_dataset, model, tokenizer, labels_pos, labels_ner, pad_token_label_id)
         logger.info(" global_step = %s, average loss = %s", global_step, tr_loss)
 
     # Saving best-practices: if you use defaults names for the model, you can reload it using from_pretrained()
