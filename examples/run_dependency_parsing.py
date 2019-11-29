86bc662dd0cb8e3ce6304a34b3fcd63c5272c93d
Tarpelite <tarpelite_chan@foxmail.com>
2019-11-29 20:09:35 +0800

    debug

--- examples/run_dependency_parsing.py
+++ examples/run_dependency_parsing.py
@@ -445,10 +445,11 @@ def main():
                                         mlp_input=args.max_seq_length, 
                                         mlp_arc_hidden=1024,
                                         mlp_lab_hidden=1024,
-                                        mlp_droput=0.1, 
+                                        # mlp_droput=0.1, 
                                         num_labels=num_labels,
                                         critierion=nn.CrossEntropyLoss(),
                                         max_len=args.max_seq_length)
+
 
     if args.local_rank == 0:
         torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
