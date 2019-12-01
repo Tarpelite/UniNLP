423e9f52ce15a90de37801d536c826e404d63b0b
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-01 16:29:52 +0800

    fix bugs

--- transformers/modelling_dependency_decoder.py
+++ transformers/modelling_dependency_decoder.py
@@ -84,7 +84,7 @@ class BiAffineParser(BertPreTrainedModel):
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-        self.encoder = RecurrentEncoder(config.hidden_size, config.hidden_size*2, 1, batch_first=True, dropout=config.hidden_dropout_prob)
+        self.encoder = RecurrentEncoder(config.hidden_size, mlp_input, 1, batch_first=True, dropout=config.hidden_dropout_prob)
         # Arc MLPs
         self.arc_mlp_h = MLP(mlp_input, mlp_arc_hidden, 2, "ReLU", mlp_dropout)
         self.arc_mlp_d = MLP(mlp_input, mlp_arc_hidden, 2, "ReLU", mlp_dropout)
