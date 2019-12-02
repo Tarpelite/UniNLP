89713efdef753fcf8f9c1d54546e3c5e0bcb8680
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-02 10:05:35 +0800

    debug

--- transformers/modelling_dependency_decoder.py
+++ transformers/modelling_dependency_decoder.py
@@ -12,13 +12,13 @@ class MLP(nn.Module):
     def __init__(self, input_size, layer_size, depth, activation, dropout):
         super(MLP, self).__init__()
         self.layers = nn.Sequential()
-        act_fn = getattr(nn, activation, inPlace=False)
+        act_fn = getattr(nn, activation)
         for i in range(depth):
             self.layers.add_module('fc_{}'.format(i),
                                     nn.Linear(input_size, layer_size))
             if activation:
                 self.layers.add_module('{}_{}'.format(activation, i),
-                                        act_fn())
+                                        act_fn(inplace=False))
             
             if dropout:
                 self.layers.add_module('dropout_{}'.format(i),
