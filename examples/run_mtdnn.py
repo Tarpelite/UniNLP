4a59c0f6fbf312d2e43616175b58efde13277338
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-05 15:51:00 +0800

    adjust learning rate for alpha

--- examples/run_mtdnn.py
+++ examples/run_mtdnn.py
@@ -70,7 +70,8 @@ def finetune(args, train_dataset, model, tokenizer, labels, pad_token_label_id, 
     optimizer_grouped_parameters = [
         {"params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
          "weight_decay": args.weight_decay},
-        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0}
+        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
+        {'params': ["alpha_pos", "alpha_ner"], 'lr': 1e-3}
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total)
@@ -341,8 +342,10 @@ def train(args, train_data_list, model, tokenizer, labels_pos, labels_ner, pad_t
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': args.weight_decay},
-        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
+        {'params': ['alpha_pos', 'alpha_ner'], 'lr':1e-3}
         ]
+    
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total)
 
