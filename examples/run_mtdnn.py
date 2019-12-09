0b1415762a034f2bcfea76b784f529259991cccd
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-09 12:52:50 +0800

    debug

--- examples/run_mtdnn.py
+++ examples/run_mtdnn.py
@@ -366,7 +366,7 @@ def train(args, train_data_list, model, tokenizer, labels_pos, labels_ner, pad_t
             {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in alpha_sets)], 'lr':args.alpha_learning_rate}
         ]
 
-        used_params = []
+        used_params = no_decay + alpha_sets
         for i in range(num_layers):
             params = []
             for param in all_parameters:
