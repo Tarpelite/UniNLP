7e6fad7c14e17c993ed8583f5562c6fb5f27cec9
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-19 19:27:08 +0800

    add the task of srl

--- transformers/modeling_bert.py
+++ transformers/modeling_bert.py
@@ -1678,6 +1678,7 @@ class AdapterBertModel(BertPreTrainedModel):
         outputs = (sequence_output, pooled_output,) + encoder_outputs[1:]  # add hidden_states and attentions if they are here
         return outputs  # sequence_output, pooled_output, (hidden_states), (attentions)
 
+
 class AdapterMTDNNModel(BertPreTrainedModel):
     def __init__(self, config, num_labels_pos, num_labels_ner, num_labels_chunking, init_last=False):
         super(AdapterMTDNNModel, self).__init__(config)
@@ -1959,3 +1960,49 @@ class MTDNNModelTaskEmbeddingT4(BertPreTrainedModel):
         if do_alpha:
             outputs = (alpha_vis,) + outputs
         return outputs  # (loss), scores, (hidden_states), (attentions)
+
+class BertForSRL(BertPreTrainedModel):
+
+    def __init__(self, config):
+        super(BertForSRL, self).__init__(config)
+        self.num_labels= config.num_labels
+
+        self.bert = BertModel(config)
+        self.dropout = config.Dropout(config.hidden_dropout_prob)
+
+        self.classifier = nn.Linear(config.hidden_size, 1, self.num_labels)
+
+        self.init_weights()
+    
+    def forward(self, input_ids=None, verb_seq_ids=None, attention_mask=None, token_type_ids=None,
+                position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
+        
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+        sequence_output = outputs[0]
+
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output, verb_seq_ids)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            # Only keep active parts of the loss
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1) == 1
+                active_logits = logits.view(-1, self.num_labels)[active_loss]
+                active_labels = labels.view(-1)[active_loss]
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), scores, (hidden_states), (attentions)
+
+
+        
