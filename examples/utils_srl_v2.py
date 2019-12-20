e58a858a7f7781f6ee4a59ab199bd8bebf32228f
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-20 17:28:44 +0800

    add srl

--- examples/utils_srl_v2.py
+++ examples/utils_srl_v2.py
@@ -230,7 +230,7 @@ def convert_examples_to_features(examples,
             logger.info("segment_ids: %s", " ".join([str(x) for x in segment_ids]))
             logger.info("label_ids: %s", " ".join([str(x) for x in label_ids]))
 
-        print(type(label_ids))
+        # print(type(label_ids))
         features.append(
                 InputFeatures(input_ids=input_ids,
                               input_mask=input_mask,
