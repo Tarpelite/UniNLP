694ab5368a904ab3e6f380e366dca4d5dafe4b5a
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-09 20:27:55 +0800

    debug

--- examples/convert.py
+++ examples/convert.py
@@ -52,12 +52,13 @@ from uninlp import MTDNNModel, BertConfig
 from pudb import set_trace
 set_trace()
 
+task_list = ["pos", "ner", "chunking", "srl", "onto_pos", "onto_ner"]
 def get_labels(labels_path):
     with open(labels_path, "r") as f:
         labels = f.read().splitlines()
     return labels
 
-def convert_model(src_path, config_path, container_path, labels_path, target_path, task):
+def convert_single_task_model(src_path, config_path, container_path, labels_path, target_path, task):
     model = torch.load(src_path)
     model = model.module if hasattr(model, "module") else model
     config = BertConfig.from_pretrained(config_path,
@@ -85,18 +86,74 @@ def convert_model(src_path, config_path, container_path, labels_path, target_pat
     torch.save(tgt_model, model_path)
     cp_command = "mv {} {}".format(model_path, src_path)
     os.system(cp_command)
+
+def convert_full_task_model(src_path, config_path, contanier_path, data_dir, target_path):
     
+    labels_list = []
+    for task in task_list:
+        path = os.path.join(data_dir, task.upper(), "labels.txt")
+        labels = get_labels(path)
+        labels_list.append(labels)
+
+    config = BertConfig.from_pretrained(config_path,
+                                        num_labels=9,
+                                        cache_dir=None,
+                                        output_hidden_state=True)
+    src_model = MTDNNModelv4.from_pretrained(src_path,
+                                            from_tf=False,
+                                            num_labels_pos=len(labels_list[0]),
+                                            num_labels_ner=len(labels_list[1]),
+                                            num_labels_chunking=len(labels_list[2]),
+                                            num_labels_srl=len(labels_list[3]),
+                                            num_labels_onto_pos=len(labels_list[4]),
+                                            num_labels_onto_ner=len(labels_list[5]),
+                                            cache_dir=None,
+                                            init_last=False,
+                                            do_adapter=False
+                                            )
+    src_model = src_model.module if hasattr(model, "module") else src_model
+
+    tgt_config = Bertconfig.from_pretrained(config_path,
+                                            num_labels=2,
+                                            cache_dir=None,
+                                            output_hidden_states=True)
+    tgt_model = MTDNNModel.from_pretrained(container_path,
+                                           from_tf=False,
+                                           config=tgt_config,
+                                           labels_list=labels_list
+                                           do_task_embedding=False,
+                                           do_alpha=False,
+                                           do_adapter=False,
+                                           num_adapter_layer=2)
+    tgt_model.bert = model.bert
+    tgt_model.dropout = model.dropout
+    for i, task in enumerate(task_list):
+        tgt_model.classifier_list[i] = getattr(src_model, "classifier_{}".format(task.lower()))
+
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
+    # tgt_model.save_pretrained(target_path)
+    model_path = os.path.join(target_path, "pytorch_model.bin")
+    torch.save(tgt_model, model_path)
+    cp_command = "mv {} {}".format(model_path, src_path)
+    os.system(cp_command)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--src_path", type=str)
-    parser.add_argument("--tgt_path", type=str)
+    parser.add_argument("--src_dir", type=str)
+    parser.add_argument("--tgt_dir", type=str)
+    parser.add_argument("--data_dir", type=str)
     parser.add_argument("--config_path", type=str)
     parser.add_argument("--container_path", type=str)
     parser.add_argument("--labels_path", type=str)
     parser.add_argument("--task", type=str)
 
     args = parser.parse_args()
-
-    convert_model(args.src_path, args.config_path, args.container_path, args.labels_path, args.tgt_path, args.task)
+    # convert single task
+    for task in task_list:
+        print("convert ", task)
+        src_path = os.path.join(args.src_dir, "{}-ft.bin".format(task))
+        label_file = os.path.join(args.data_dir, task.upper(), "labels.txt")
+        convert_single_task_model(src_path, args.config_path, args.container_path, label_file, args.tgt_dir, task)
+    convert_full_task_model(args.src_path, args.config_path, args.container_path, args.data_dir, args.tgt_dir)
