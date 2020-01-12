046296e2606678c60bd1d3d35da7a54fdd241148
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-12 17:03:55 +0800

    debug

--- examples/run_dependency_parsing.py
+++ examples/run_dependency_parsing.py
@@ -139,6 +139,8 @@ def train(args, train_dataset, model, tokenizer, labels, pad_token_label_id):
             batch = tuple(t.to(args.device) for t in batch)
             inputs = {"input_ids": batch[0],
                       "attention_mask": batch[1],
+                      "heads":batch[4],
+                      "labels":batch[3]
                       }
             if args.model_type != "distilbert":
                 inputs["token_type_ids"]: batch[2] if args.model_type in ["bert", "xlnet"] else None  # XLM and RoBERTa don"t use segment_ids
