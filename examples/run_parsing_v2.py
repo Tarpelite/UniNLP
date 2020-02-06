b9122421be6756585c08c680655bc5a663bbecb5
Tarpelite <tarpelite_chan@foxmail.com>
2020-02-06 13:43:09 +0800

    debug

--- examples/run_parsing_v2.py
+++ examples/run_parsing_v2.py
@@ -510,7 +510,7 @@ def main():
         logger.info("Evaluate the following checkpoints: %s", checkpoints)
         for checkpoint in checkpoints:
             global_step = checkpoint.split("-")[-1] if len(checkpoints) > 1 else ""
-            model = model_class.from_pretrained(checkpoint, mlp_dim=args.mlp_dim)
+            model = model_class.from_pretrained(checkpoint, mlp_dim=args.mlp_dim, num_labels=num_labels)
             model.to(args.device)
             result, _ = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode="dev", prefix=global_step)
             if global_step:
